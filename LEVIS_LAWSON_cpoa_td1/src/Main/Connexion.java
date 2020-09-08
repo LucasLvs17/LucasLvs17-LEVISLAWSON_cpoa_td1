260f1daf91094b485663a09150f5d801c3d920ba
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-09-08 10:34:53 +0200

    ajout connexion dans un package "main"

---
+++ LEVIS_LAWSON_cpoa_td1/src/Main/Connexion.java
@@ -0,0 +1,17 @@
+package Main;
+import java.sql.*;
+
+public class Connexion {
+	public static Connection CreateConnexion(){
+		String url= "jdbc:mysql//devbdd.iutmetz.univ-lorraine.fr:3306/levis5u_cpoa?serverTimezone=Europe/Paris";
+		String login = "levis5u_appli";
+		String pwd = "31801108";
+		Connection maConnexion = null;
+		try {
+			maConnexion = DriverManager.getConnection(url, login, pwd);
+		} catch(SQLException sqle) {
+			System.out.println("Erreur connexion" + sqle.getMessage());		
+		}
+		return maConnexion;
+	} 
+}
