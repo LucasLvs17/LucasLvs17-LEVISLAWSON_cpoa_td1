5ef4087f6c1c53496e07e6182be9fb67e056f7d0
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-09-15 11:40:36 +0200

    Modif ListeMemoireCategorie

--- LEVIS_LAWSON_cpoa_td1/src/DAO/CategorieDAO.java
+++ LEVIS_LAWSON_cpoa_td1/src/DAO/CategorieDAO.java
@@ -1,7 +1,15 @@
 package DAO;
+import java.util.ArrayList;
+
 import POJO.*;
 
 public interface CategorieDAO extends DAO<Categorie>{
+	
+
 	public abstract Categorie getById(int id1, int id2);
+
+	ArrayList<Categorie> findAll();
+
+	Categorie getById(int id);
 
 }
