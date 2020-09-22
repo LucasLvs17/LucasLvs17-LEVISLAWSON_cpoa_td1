1d68359e87e4f27639cf85bd21811686a587447d
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-09-22 09:10:07 +0200

    Correction pb dans MYSQLDAOFactory Pb à corriger dans MySQLCategorieDAO

--- LEVIS_LAWSON_cpoa_td1/src/DAO/CategorieDAO.java
+++ LEVIS_LAWSON_cpoa_td1/src/DAO/CategorieDAO.java
@@ -8,7 +8,6 @@ public interface CategorieDAO extends DAO<Categorie>{
 
 	public abstract Categorie getById(int id1, int id2);
 
-	ArrayList<Categorie> findAll();
 
 	Categorie getById(int id);
 
--- LEVIS_LAWSON_cpoa_td1/src/POJO/Categorie.java
+++ LEVIS_LAWSON_cpoa_td1/src/POJO/Categorie.java
@@ -14,6 +14,7 @@ public class Categorie {
 		
 	}
 
+
 	public int getIdCategorie() {
 		return id_categorie;
 	}
