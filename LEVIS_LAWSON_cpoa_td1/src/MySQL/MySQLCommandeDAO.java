4160c24489a07c04422b09905d55f45aebe86fab
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-09-24 16:51:36 +0200

    Fin de MySQLCommandeDAO

--- LEVIS_LAWSON_cpoa_td1/src/MySQL/MySQLCommandeDAO.java
+++ LEVIS_LAWSON_cpoa_td1/src/MySQL/MySQLCommandeDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import DAO.*;
 import Main.Connexion;
@@ -88,7 +89,7 @@ public class MySQLCommandeDAO implements CommandeDAO{
 			Commande c = new Commande();
 			c.setId_commande(id_commande);
 			if(res.next()) {
-				c.setDate_commande(res.getDate("date_commande"));
+				c.setDate_commande(res.getDate("date_commande").toLocalDate());
 				c.setId_client(res.getInt("id_client"));
 			}
 			return c;
@@ -99,8 +100,73 @@ public class MySQLCommandeDAO implements CommandeDAO{
 	}
 
 	
+	public ArrayList<Commande> getByIdCommande(int id_commande){
+		Connection laCo = Connexion.CreateConnexion();
+		try {
+			PreparedStatement requete = laCo.prepareStatement("SELECT * FROM COMMANDE WHERE id_commande = ?");
+			requete.setInt(1, id_commande);
+			ResultSet res = requete.executeQuery();
+			laCo.close();
+			ArrayList<Commande> a = new ArrayList<Commande>();
+			Commande c = new Commande();
+			while(res.next()) {
+				c.setId_commande(res.getInt("id_commande"));
+				c.setDate_commande(res.getDate("date_commande").toLocalDate());
+				c.setId_client(res.getInt("id_client"));
+				a.add(c);
+			}
+			return a;
+		}catch(SQLException e){
+			System.out.println("Pb select" + e.getMessage());
+			return null;
+		}
+	}
 
+	public ArrayList<Commande> getByDateDebut(LocalDate date_commande){
+		Connection laCo = Connexion.CreateConnexion();
+		try {
+			PreparedStatement requete = laCo.prepareStatement("SELECT * FROM Abonnement WHERE date_debut = ?");
+			requete.setDate(1, java.sql.Date.valueOf(date_commande));
+			ResultSet res = requete.executeQuery();
+			laCo.close();
+			ArrayList<Commande> a = new ArrayList<Commande>();
+			Commande c = new Commande();
+			while(res.next()) {
+				c.setDate_commande(res.getDate("date_debut").toLocalDate());
+				c.setId_client(res.getInt("id_client"));
+				c.setId_commande(res.getInt("id_commande"));
+				a.add(c);
+			}
+			return a;
+			
+		}catch(SQLException e){
+			System.out.println("Pb select" + e.getMessage());
+			return null;
+		}
+	}
 
+	
+	public ArrayList<Commande> getByIdClient(int id_client){
+		Connection laCo = Connexion.CreateConnexion();
+		try {
+			PreparedStatement requete = laCo.prepareStatement("SELECT * FROM COMMANDE WHERE id_commande = ?");
+			requete.setInt(1, id_client);
+			ResultSet res = requete.executeQuery();
+			laCo.close();
+			ArrayList<Commande> a = new ArrayList<Commande>();
+			Commande c = new Commande();
+			while(res.next()) {
+				c.setId_client(res.getInt("id_client"));
+				c.setId_commande(res.getInt("id_commande"));
+				c.setDate_commande(res.getDate("date_commande").toLocalDate());
+				a.add(c);
+			}
+			return a;
+		}catch(SQLException e){
+			System.out.println("Pb select" + e.getMessage());
+			return null;
+		}
+	}
 	
 	
 }
