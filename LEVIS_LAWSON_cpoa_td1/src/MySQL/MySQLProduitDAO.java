bea5f3920ef44c0523a8da5fc07567628118259a
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-09-29 11:39:04 +0200

    Les Tests

--- LEVIS_LAWSON_cpoa_td1/src/MySQL/MySQLProduitDAO.java
+++ LEVIS_LAWSON_cpoa_td1/src/MySQL/MySQLProduitDAO.java
@@ -7,9 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import DAO.*;
 import Main.Connexion;
-import POJO.Commande;
 import POJO.Produit;
 
 public class MySQLProduitDAO {
--- LEVIS_LAWSON_cpoa_td1/src/TestsMySQL/TestMySQLCategorie.java
+++ LEVIS_LAWSON_cpoa_td1/src/TestsMySQL/TestMySQLCategorie.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import DAO.CategorieDAO;
 import POJO.Categorie;
 import MySQL.MySQLCategorieDAO;
 
