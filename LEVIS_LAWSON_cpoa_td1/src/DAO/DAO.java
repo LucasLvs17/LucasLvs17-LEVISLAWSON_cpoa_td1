e03242c5d7ff9f7b03142c4d21eeb4b30067fe9c
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-10-13 08:36:55 +0200

    un truc est modif mais je sais plus où

--- LEVIS_LAWSON_cpoa_td1/src/DAO/DAO.java
+++ LEVIS_LAWSON_cpoa_td1/src/DAO/DAO.java
@@ -1,7 +1,5 @@
 package DAO;
 
-import POJO.Client;
-
 public interface DAO<T> {
 	public abstract T getById(int id) throws Exception; //jsp a quoi ça sert mais bon
 	public abstract boolean create(T object) throws Exception;
